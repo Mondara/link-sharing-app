1bd435d65331b5d35010e9ec462be9c6c559208c
Mondara <52889506+Mondara@users.noreply.github.com>
2023-08-13 00:11:04 +1000

    Initialised Project.

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  theme: {
+    colors: {
+      purple: "#633CFF",
+      "light-purple": "#EFEBFF",
+      "purple-hover": "#BEADFF",
+      "dark-gray": "#333333",
+      gray: "737373",
+      "light-gray": "#FAFAFA",
+      "border-gray": "#D9D9D9",
+      red: "#FF3939",
+    },
+  },
+  plugins: [],
+};
