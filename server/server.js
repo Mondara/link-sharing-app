26bd12838494b4e90d4ffe649e662905f9a45c13
Mondara <mondara.t@hotmail.com>
2023-10-05 13:31:43 +1100

    Fixed Corse Allow Origin URL

--- server/server.js
+++ server/server.js
@@ -12,7 +12,7 @@ connectDB();
 const app = express();
 
 const corsOptions = {
-  origin: "http://127.0.0.1:5173",
+  origin: "https://link-app-u33e.onrender.com",
 };
 
 app.use(cors(corsOptions));
